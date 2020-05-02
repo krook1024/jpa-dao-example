2be3ddea3b8d22651e6c293babead67184276b49
Molnár Antal Albert <krook1024@gmail.com>
2020-05-02 13:27:52 +0200

    Created BookGenerator in the book package to generate random books

---
+++ src/main/java/book/BookGenerator.java
@@ -0,0 +1,29 @@
+package book;
+
+import book.model.Book;
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class BookGenerator {
+    private static final Faker faker = new Faker();
+
+    public Book getRandomBook() {
+        Book book = new Book();
+        book.setIsbn13(faker.code().isbn13());
+        book.setAuthor(faker.book().author());
+        book.setTitle(faker.book().title());
+        book.setFormat(faker.options().option(Book.Format.HARDBACK, Book.Format.PAPERBACK));
+        book.setPublisher(faker.book().publisher());
+        book.setPublicationDate(toLocalDate(faker.date().birthday()));
+        book.setPages((int) Math.abs(faker.number().randomNumber(3, false)));
+        book.setAvailable(faker.bool().bool());
+        return book;
+    }
+
+    private LocalDate toLocalDate(Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+}
